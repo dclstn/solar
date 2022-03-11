8be7466591ea4de021104357345c761c630d9f69
dclstn <donnieacolston@gmail.com>
2022-03-11 04:09:58 +0000

    move server to own file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 /* eslint-disable no-console */
 import Discord from 'discord.js';
 import * as dotenv from 'dotenv';
-import fastifyJwt from 'fastify-jwt';
-import {FastifyReply} from 'fastify';
 import chalk from 'chalk';
-import App from './server.js';
 import Sentry from './sentry.js';
 
-import authRoute from './routes/auth/index.js';
-import stripeRoute from './routes/stripe/index.js';
-import userRoute from './routes/user/index.js';
-
 dotenv.config();
-const PORT = 8000;
 
 const manager = new Discord.ShardingManager('./dist/app.js', {
   totalShards: 'auto',
@@ -27,40 +19,6 @@ manager.on('shardCreate', (shard) => {
   console.log(`${chalk.cyan(`[Shards]`)} Launched shard ${shard.id}`);
 });
 
-(async () => {
-  await manager.spawn(); // affirm the bot comes online
+manager.spawn();
 
-  App.register(fastifyJwt, {secret: process.env.JWT_SECRET});
-
-  App.register(authRoute);
-  App.register(userRoute);
-  App.register(stripeRoute);
-
-  App.decorate('authenticate', async (request, response: FastifyReply) => {
-    try {
-      await request.jwtVerify();
-    } catch (err) {
-      response.status(401).send('Unauthorized');
-    }
-  });
-
-  App.setErrorHandler(async (error, request, reply) => {
-    Sentry.captureException(error);
-    reply.status(500).send({message: 'something went wrong'});
-  });
-
-  App.listen(PORT, (error) => {
-    if (error) {
-      console.error(error);
-      process.exit(1);
-    }
-
-    console.log(`${chalk.grey('[Fastify]')} Listening on port ${PORT}`);
-
-    Sentry.addBreadcrumb({
-      category: 'server',
-      message: `Listening on port ${PORT}`,
-      level: Sentry.Severity.Info,
-    });
-  });
-})();
+import('./server.js');
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,47 @@
-import fastify from 'fastify';
+/* eslint-disable no-console */
+import fastify, {FastifyReply} from 'fastify';
+import fastifyJwt from 'fastify-jwt';
+import chalk from 'chalk';
+import Sentry from './sentry.js';
+import authRoute from './routes/auth/index.js';
+import stripeRoute from './routes/stripe/index.js';
+import userRoute from './routes/user/index.js';
 
 const App = fastify();
+const PORT = 8000;
+
+App.register(fastifyJwt, {secret: process.env.JWT_SECRET});
+
+App.register(authRoute);
+App.register(userRoute);
+App.register(stripeRoute);
+
+App.decorate('authenticate', async (request, response: FastifyReply) => {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    response.status(401).send('Unauthorized');
+  }
+});
+
+App.setErrorHandler(async (error, request, reply) => {
+  Sentry.captureException(error);
+  reply.status(500).send({message: 'something went wrong'});
+});
+
+App.listen(PORT, (error) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  console.log(`${chalk.grey('[Fastify]')} Listening on port ${PORT}`);
+
+  Sentry.addBreadcrumb({
+    category: 'server',
+    message: `Listening on port ${PORT}`,
+    level: Sentry.Severity.Info,
+  });
+});
 
 export default App;
