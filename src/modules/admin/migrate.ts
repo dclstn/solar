a2ebec4544c5dfe3cd987232b98113573d3d5a63
dclstn <donnieacolston@gmail.com>
2022-03-17 04:18:21 +0000

    attempt to ammend

--- src/modules/admin/migrate.ts
+++ src/modules/admin/migrate.ts
@@ -1,55 +1,27 @@
 import type {CommandInteraction} from 'discord.js';
 import _ from 'lodash';
-import User from '../../database/user/index.js';
-import kingdoms from './kingdoms.js';
 import Group from '../../database/group/index.js';
-import users from './users.js';
+import User from '../../database/user/index.js';
 import Sentry from '../../sentry.js';
 
 export default async function migrate(interaction: CommandInteraction) {
   try {
     interaction.reply('Migrated');
 
-    const userObj = {};
-    _.uniqBy(users, 'discordId')
-      .filter((user) => user.lastKnownUsername != null)
-      .forEach(async (user) => {
-        userObj[user._id.$oid] = {
-          username: user.lastKnownUsername,
-          discordId: user.discordId,
-        };
-      });
+    const groups = await Group.find({});
 
-    const bulkWrite = [];
-    // @ts-ignore
-    for (const {username, discordId} of Object.values(userObj)) {
-      bulkWrite.push({
-        updateOne: {
-          filter: {
-            discordId: parseInt(discordId, 10),
+    for await (const group of groups) {
+      // eslint-disable-next-line no-continue
+      if (group.users.length === 0) continue;
+      await User.updateMany(
+        {
+          _id: {
+            $in: group.users.map(({user}) => user),
           },
-          // If you wanted it to be incremented rather than replace the field, then try `$inc` instead of `$set`.
-          update: {$set: {discordId: parseInt(discordId, 10), username}},
-          upsert: true,
         },
-      });
+        {group: group._id}
+      );
     }
-    await User.collection.bulkWrite(bulkWrite);
-
-    kingdoms.forEach(async (kingdom) => {
-      const group = await Group.findOne({name: kingdom.name});
-
-      if (userObj[kingdom.king.$oid] == null || group == null) {
-        return;
-      }
-
-      const userIds = [
-        ...kingdom.users.map((user) => parseInt(userObj[user.$oid].discordId, 10)),
-        parseInt(userObj[kingdom.king.$oid].discordId, 10),
-      ];
-
-      await User.updateMany({discordId: {$in: userIds}}, {group: group._id});
-    });
   } catch (err) {
     Sentry.captureException(err);
   }
