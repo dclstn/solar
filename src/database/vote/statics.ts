9a685ffd5e0cc8783a570e454c5b3b011c2edfff
dclstn <donnieacolston@gmail.com>
2022-03-19 08:55:20 +0000

    fix Long

--- src/database/vote/statics.ts
+++ src/database/vote/statics.ts
@@ -1,9 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 
+import Long from 'long';
+
 export async function get(discordId) {
   return this.findOneAndUpdate(
     {
-      discordId,
+      discordId: Long.fromString(discordId),
     },
     {},
     {
