5ba322629e35fd4740d1c446bcbe55e1066ddd0b
dclstn <donnieacolston@gmail.com>
2022-03-20 20:55:30 +0000

    update symbol list and amount of generators

--- src/modules/games/spin.ts
+++ src/modules/games/spin.ts
@@ -5,9 +5,9 @@ import ResponseError from '../../utils/error.js';
 import Sentry from '../../sentry.js';
 
 import {numberWithCommas, slotsWin, slotsLose, warning} from '../../utils/embed.js';
-import { SlotItems, Item } from '../../utils/items.js';
+import {SLOT_ITEMS, Item} from '../../utils/items.js';
 
-const SymbolList = Object.values(SlotItems);
+const SYMBOL_LIST = Object.values(SLOT_ITEMS);
 
 export default async function spinSlots(interaction: CommandInteraction) {
   const wager = interaction.options.getInteger('wager');
@@ -16,20 +16,20 @@ export default async function spinSlots(interaction: CommandInteraction) {
   for (let i = 0; i < 5; i += 1) {
     symbols.push([]);
     for (let j = 0; j < 3; j += 1) {
-        symbols[i].push(SymbolList[Math.floor(Math.random() * SymbolList.length)]);
+      symbols[i].push(SYMBOL_LIST[Math.floor(Math.random() * SYMBOL_LIST.length)]);
     }
   }
 
-  // It would probably be more efficient on average for larger arrays to use a hashmap to store counts, but this 
+  // It would probably be more efficient on average for larger arrays to use a hashmap to store counts, but this
   // list will only ever be 3 items long, so it doesn't really matter too much I guess. Might even be able to do it
-  // with only 3 checks (compare 0 & 1, 1 & 2, 2 & 0 and if only one is true, then that's a x2, if all are true, that's 
+  // with only 3 checks (compare 0 & 1, 1 & 2, 2 & 0 and if only one is true, then that's a x2, if all are true, that's
   // a x3, and if none are true, then there is no combo), but I'll get this done and then think about it for a bit and make sure it checks out after.
   // - Josh
-  // 
+  //
   // 1. Of middle row (symbols[2])
   // 2. Map each item in middle row to the number of occurences of that item in the row
   // 3. Get max of the new list of counts
-  const multiplier = Math.max(...symbols[2].map((item, i, arr) => arr.filter(_item => _item.id === item.id).length));
+  const multiplier = Math.max(...symbols[2].map((item, i, arr) => arr.filter((_item) => _item.id === item.id).length));
 
   const lock = await redlock.acquire([userLock(interaction.user)], 1000);
 
@@ -37,22 +37,22 @@ export default async function spinSlots(interaction: CommandInteraction) {
     const user = await User.get(interaction.user);
 
     if (multiplier > 1) {
-        user.updateBalance(wager * (multiplier - 1));
-        await user.save();
+      user.updateBalance(wager * (multiplier - 1));
+      await user.save();
 
-        interaction.reply({
-          ephemeral: true,
-          embeds: [slotsWin(symbols, multiplier, numberWithCommas(wager * multiplier))],
-        });
-      } else {
-        user.updateBalance(wager * -1);
-        await user.save();
+      interaction.reply({
+        ephemeral: true,
+        embeds: [slotsWin(symbols, multiplier, numberWithCommas(wager * multiplier))],
+      });
+    } else {
+      user.updateBalance(wager * -1);
+      await user.save();
 
-        interaction.reply({
-          ephemeral: true,
-          embeds: [slotsLose(symbols, numberWithCommas(wager))],
-        });
-      }
+      interaction.reply({
+        ephemeral: true,
+        embeds: [slotsLose(symbols, numberWithCommas(wager))],
+      });
+    }
   } catch (err) {
     if (err instanceof ResponseError) {
       interaction.reply({embeds: [warning(err.message)], ephemeral: true});
--- src/utils/items.ts
+++ src/utils/items.ts
@@ -408,7 +408,7 @@ export const BUYABLE_ITEMS = Object.values(Items).filter((item) => item.buyable)
 export const DEFAULT_BUYABLE_ITEMS = BUYABLE_ITEMS.slice(0, 24);
 export const DEFAULT_ITEMS = Object.values(Items).slice(0, 24);
 
-export const SlotItems = BUYABLE_ITEMS;
+export const SLOT_ITEMS = BUYABLE_ITEMS.slice(0, 4);
 
 export function findById(itemId: string): Item {
   return Items[itemId];
