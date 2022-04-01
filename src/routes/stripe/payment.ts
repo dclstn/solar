58dc869d9926c22705620db7fa065ef0c4292656
dclstn <donnieacolston@gmail.com>
2022-04-01 01:52:16 +0100

    fulfillments fix

--- src/routes/stripe/payment.ts
+++ src/routes/stripe/payment.ts
@@ -6,32 +6,27 @@ import client from '../../client.js';
 
 export const FULLFILLMENTS = {
   [PaymentIds.HANDFUL_OF_GEMS]: async (session: Stripe.Checkout.Session) => {
-    const discordUser = await client.users.fetch(session.client_reference_id);
-    const user = await User.get(discordUser);
+    const user = await User.findOne({discordId: session.client_reference_id});
     user.addFunds(100);
     await user.save();
   },
   [PaymentIds.HANDFUL_OF_GEMS]: async (session: Stripe.Checkout.Session) => {
-    const discordUser = await client.users.fetch(session.client_reference_id);
-    const user = await User.get(discordUser);
+    const user = await User.findOne({discordId: session.client_reference_id});
     user.addFunds(300);
     await user.save();
   },
   [PaymentIds.BAG_OF_GEMS]: async (session: Stripe.Checkout.Session) => {
-    const discordUser = await client.users.fetch(session.client_reference_id);
-    const user = await User.get(discordUser);
+    const user = await User.findOne({discordId: session.client_reference_id});
     user.addFunds(900);
     await user.save();
   },
   [PaymentIds.BUTTLOAD_OF_GEMS]: async (session: Stripe.Checkout.Session) => {
-    const discordUser = await client.users.fetch(session.client_reference_id);
-    const user = await User.get(discordUser);
+    const user = await User.findOne({discordId: session.client_reference_id});
     user.addFunds(2000);
     await user.save();
   },
   [PaymentIds.TEST_BUTTLOAD_OF_GEMS]: async (session: Stripe.Checkout.Session) => {
-    const discordUser = await client.users.fetch(session.client_reference_id);
-    const user = await User.get(discordUser);
+    const user = await User.findOne({discordId: session.client_reference_id});
     user.addFunds(2000);
     await user.save();
   },
