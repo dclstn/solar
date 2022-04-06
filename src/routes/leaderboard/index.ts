760b06fec05e1fcab6484a9f302489cee0b045bb
dclstn <donnieacolston@gmail.com>
2022-04-06 10:28:07 +0100

    api update

--- src/routes/leaderboard/index.ts
+++ src/routes/leaderboard/index.ts
@@ -1,9 +1,26 @@
 import type {FastifyRequest, FastifyReply} from 'fastify';
-import {TopUserMoneyModel} from '../../database/user/aggregation-models.js';
+import {TopCPHUserModel, TopUserMoneyModel} from '../../database/user/aggregation-models.js';
+
+const types = {
+  CPH: 'cph',
+  MONEY: 'money',
+};
 
 export default (fastify, opts, done) => {
   fastify.get('/api/leaderboards', async (request: FastifyRequest, response: FastifyReply) => {
-    const users = await TopUserMoneyModel.find({}).limit(5);
+    const {type}: any = request.query;
+
+    let users = null;
+    switch (type) {
+      case types.MONEY:
+        users = await TopUserMoneyModel.find({}).lean().limit(10);
+        break;
+      case types.CPH:
+      default:
+        users = await TopCPHUserModel.find({}).lean().limit(10);
+        break;
+    }
+
     response.send({users});
   });
 
