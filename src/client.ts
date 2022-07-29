412823e5504fd66bb163f0d71b6a8d12c79dcdfc
dclstn <donnieacolston@gmail.com>
2022-07-29 15:20:03 +0100

    update discord.js

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,9 @@
-import {Client, Intents} from 'discord.js';
+import {Client, IntentsBitField} from 'discord.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({ intents: [IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages] });
 
 client.login(process.env.DISCORD_TOKEN);
 
