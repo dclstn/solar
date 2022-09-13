0c41ad319e70faad8c1337eb64ae8166a63da8bc
dclstn <donnieacolston@gmail.com>
2022-09-13 17:28:08 +0100

    fix linting check

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,11 +1,13 @@
 import {OAuth2Namespace} from 'fastify-oauth2';
 
 declare module 'fastify' {
+  interface FastifyRequest {
+    // @ts-ignore
+    user: unknown;
+  }
+
   interface FastifyInstance {
     discordOAuth2: OAuth2Namespace;
     authenticate: (request: FastifyRequest) => void;
-  }
-  interface FastifyRequest {
-    user: any;
   }
 }
