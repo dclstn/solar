82a4451314defea9b1341d9697a82b49b783f92e
Donnie <43322006+dclstn@users.noreply.github.com>
2022-10-06 21:52:35 +0100

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,5 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log('Connected to Mongoose');
 
 export default mongoose.createConnection(process.env.MONGO_URI);
