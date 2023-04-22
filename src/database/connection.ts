8eaa04163f3b0aba39943c457fba2ea79ce90c46
Donnie <donnieacolston@gmail.com>
2023-04-22 13:37:15 +0100

    expose mongo uri for testing

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,4 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 console.log('Connected to Mongoose');
 
+console.log(process.env.MONGO_URI);
+
 export default mongoose.createConnection(process.env.MONGO_URI);
